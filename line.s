4326b90724e67c781b7072d934712d01898a6a79
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-31 04:59:42 -0700

    first commit

--- line.s
+++ line.s
@@ -5,3 +5,18 @@
 # 
 #  Description: 
 #    Stores line of text in array and duplicates it into another array
+          .data
+LINELEN:  .ascii    40
+intro:    .asciiz   "String by P. Nguyen"
+prompt:   .asciiz   "Enter text? Line 1 "
+str1:     .space    40
+str2:     .space    40
+
+main:
+          li   $v0, 4
+          la   $a0, intro
+          syscall
+
+          li   $v0, 4
+          la   $a0, prompt
+          syscall
