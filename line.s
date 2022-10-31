116b99988b2fbb88b5c2e8e355ad9a545bde86df
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-31 01:38:32 -0700

    first commit

---
+++ line.s
@@ -0,0 +1,7 @@
+#  Name:   Nguyen, Peter
+#  Homework:  # 3
+#  Due:        date 
+#  Course:  cs-2640-04-f22 
+# 
+#  Description: 
+#    Stores line of text in array and duplicates it into another array
